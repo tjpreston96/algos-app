50dcee7ec3d6b186ec4c2ca063b57fbb96435b8e
tpreston96 <tpreston96@gmail.com>
2021-04-28 00:37:53 -0500

    Palindrome functionality

--- src/pages/strings/Palindrome/Palindrome.jsx
+++ src/pages/strings/Palindrome/Palindrome.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Palindrome = () => {
   const [str, setStr] = useState(null);
+  const [result, setResult] = useState(null);
 
   const onInputChange = (e) => {
     setStr(e.target.value);
@@ -13,14 +14,28 @@ const Palindrome = () => {
   };
 
   const isPalindrome = () => {
-    return str === str.split("").reverse().join("");
+    setResult(
+      str.toLowerCase() === str.toLowerCase().split("").reverse().join("")
+        ? "True"
+        : "False"
+    );
   };
+
   return (
     <>
       <div className="margin-div">
         <div className="page">
-          <h1>Palindrome</h1>
-          <form onSubmit={onFormSubmit}></form>
+          <h1>Palindrome Tester</h1>
+          <form onSubmit={onFormSubmit}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Please enter a {String}"
+              onChange={onInputChange}
+            />
+          </form>
+          <h4>Output</h4>
+          {result ? result : `waiting on input...`}
         </div>
       </div>
     </>
