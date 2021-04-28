d98b579e2651b2667c9d36e89d52e5955a7413ee
tpreston96 <tpreston96@gmail.com>
2021-04-27 23:54:12 -0500

    Palindrome set up

--- src/pages/strings/Palindrome/Palindrome.jsx
+++ src/pages/strings/Palindrome/Palindrome.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Palindrome = () => {
+  const [str, setStr] = useState(null);
+
+  const onInputChange = (e) => {
+    setStr(e.target.value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    isPalindrome();
+  };
+
+  const isPalindrome = () => {
+    return str === str.split("").reverse().join("");
+  };
   return (
     <>
       <div className="margin-div">
         <div className="page">
           <h1>Palindrome</h1>
+          <form onSubmit={onFormSubmit}></form>
         </div>
       </div>
     </>
