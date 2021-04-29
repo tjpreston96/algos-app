eb5af6dbab887dec04687a081307132a1e7931c8
tpreston96 <tpreston96@gmail.com>
2021-04-29 08:23:54 -0500

    reversal functionality

--- src/pages/numbers/IntReversal/IntReversal.jsx
+++ src/pages/numbers/IntReversal/IntReversal.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IntReversal = () => {
+  const [num, setNum] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const onInputChange = (e) => {
+    setNum(e.target.value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    reversal();
+  };
+
+  const reversal = () => {
+    const reversed = parseInt(num.toString().split("").reverse().join(""));
+
+    if (num < 0) {
+      setResult(reversed * -1);
+    } else setResult(reversed);
+  };
+
   return (
     <>
       <div className="margin-div">
         <div className="page">
           <h1>Integer Reversal</h1>
+          <form onSubmit={onFormSubmit}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Please enter a {number}"
+              onChange={onInputChange}
+            />
+          </form>
+          <h4>Output</h4>
+          {result}
         </div>
       </div>
     </>
