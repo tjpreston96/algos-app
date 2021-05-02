f54822c2371aa2bedda1f4fd9185f1016a78ebd7
tpreston96 <tpreston96@gmail.com>
2021-05-02 01:08:02 -0500

    maxChar functionality

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from "./pages/Home/Home";
 import Fizzbuzz from "./pages/numbers/Fizzbuzz/Fizzbuzz";
 import IntReversal from "./pages/numbers/IntReversal/IntReversal";
 import Anagrams from "./pages/strings/Anagrams/Anagrams";
-import MaxChar from "./pages/strings/MaxChar/MaxChar";
+import MaxChars from "./pages/strings/MaxChar/MaxChar";
 import Palindrome from "./pages/strings/Palindrome/Palindrome";
 import StrReversal from "./pages/strings/StrReversal/StrReversal";
 import Vowels from "./pages/strings/Vowels/Vowels";
@@ -23,7 +23,7 @@ function App() {
       <Route exact path="/int-reversal" render={() => <IntReversal />} />
       {/* ---- String Algo Routes ---- */}
       <Route exact path="/anagrams" render={() => <Anagrams />} />
-      <Route exact path="/maxChar" render={() => <MaxChar />} />
+      <Route exact path="/maxChar" render={() => <MaxChars />} />
       <Route exact path="/palindrome" render={() => <Palindrome />} />
       <Route exact path="/str-reversal" render={() => <StrReversal />} />
       <Route exact path="/vowels" render={() => <Vowels />} />
--- src/pages/strings/MaxChar/MaxChar.jsx
+++ src/pages/strings/MaxChar/MaxChar.jsx
@@ -1,9 +1,43 @@
 import React, { useState } from "react";
 
-const MaxChar = () => {
+const MaxChars = () => {
   const [str, setStr] = useState(null);
   const [result, setResult] = useState(null);
 
+  const onInputChange = (e) => {
+    setStr(e.target.value);
+  };
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    maxChar();
+  };
+
+  const maxChar = () => {
+    let charMap = {};
+    let max = 0;
+    let maxChar = "";
+
+    // Character Map
+    for (let char of str) {
+      if (charMap[char]) {
+        charMap[char]++;
+      } else {
+        charMap[char] = 1;
+      }
+    }
+
+    //   iterate charMap to find most frequently used value
+    //   'for in' syntax (ES2015) because charMap is an object
+    for (let char in charMap) {
+      if (charMap[char] > max) {
+        max = charMap[char];
+        maxChar = char;
+      }
+    }
+
+    return setResult(maxChar);
+  };
   return (
     <>
       <div className="margin-div">
@@ -23,4 +57,4 @@ const MaxChar = () => {
   );
 };
 
-export default MaxChar;
+export default MaxChars;
