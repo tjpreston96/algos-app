a9b3e7c693ef9e9cdb4e3a3b3613191dfefdb33d
tpreston96 <tpreston96@gmail.com>
2021-05-02 00:08:12 -0500

    anagram input setup

--- src/pages/strings/Anagrams/Anagrams.jsx
+++ src/pages/strings/Anagrams/Anagrams.jsx
@@ -5,12 +5,36 @@ const Anagrams = () => {
   const [str2, setStr2] = useState(null);
   const [result, setResult] = useState(null);
 
+  const onStr1Change = (e) => {
+    setStr1(e.target.value);
+  };
+
+  const onStr2Change = (e) => {
+    setStr2(e.target.value);
+  };
 
   return (
     <>
       <div className="margin-div">
         <div className="page">
           <h1>Anagrams</h1>
+          <form>
+            <label> String 1 </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Please enter a {string}"
+              onChange={onStr1Change}
+            />
+            <br />
+            <label> String 2 </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Please enter a {string}"
+              onChange={onStr2Change}
+            />
+          </form>
         </div>
       </div>
     </>
