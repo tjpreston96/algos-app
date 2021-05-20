af69dfef4b25c50a977c977302d8048dff2d32af
tpreston96 <tpreston96@gmail.com>
2021-05-19 23:36:32 -0500

    home update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
       <div className="margin-div">
         <div className="page">
           <h1>Welcome to AlgoApp</h1>
+          <h4>This application is a way for me to practice utilizing standard JavaScript algorithms while maintaining state in a React environment.</h4>
           <Link to="https://www.linkedin.com/in/tylerjpreston/">LinkedIn</Link>
           <Link to="https://github.com/tjpreston96">GitHub</Link>
         </div>
